7fa6b3ab918c7d752db755953ae6fb9b565e73ad
UO275688 <uo275688@uniovi.es>
2022-02-13 20:27:09 +0100

    SDI-2122-805-2.1-Create Spring project.

---
+++ src/main/java/com/uniovi/sdi2122805labspring/Sdi2122805LabSpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2122805labspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2122805LabSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2122805LabSpringApplication.class, args);
+    }
+
+}
