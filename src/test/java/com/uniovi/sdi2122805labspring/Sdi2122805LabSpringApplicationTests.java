5a2f82e13e97710cd9c8e5430c46905eb5b0208e
UO275688 <uo275688@uniovi.es>
2022-03-03 10:56:25 +0100

    Solución desarrollo de pruebas con Selenium

--- src/test/java/com/uniovi/sdi2122805labspring/Sdi2122805LabSpringApplicationTests.java
+++ src/test/java/com/uniovi/sdi2122805labspring/Sdi2122805LabSpringApplicationTests.java
@@ -275,7 +275,6 @@ class Sdi2122805LabSpringApplicationTests {
         PO_PrivateView.clickOption(driver, "logout", "text", loginText);
     }
 
-
     //Eliminar una nota
     @Test
     @Order(17)
